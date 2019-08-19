46c340cc422005838e662911b133ac966ff5706f
Bhanuka <bhanukamax@gmail.com>
2019-08-19 20:04:48 +0530

    added electron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const root = document.querySelector("#root")
 const el = document.createElement('p')
 
-el.innerHTML = 'what a text this is the again and again'
+el.innerHTML = 'what a what'
 
 root.append(el)
